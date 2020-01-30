ecb89d3658f9276f15862a6c09ce8afde4c005a6
miguel.palao <miguel.palao@sm2baleares.es>
2020-01-30 13:52:17 +0100

    feat: Attack functionality have been added
    Now the player and the monster can attack each other and see who have
    won

--- control-04-attack-function/assets/scripts/app.js
+++ control-04-attack-function/assets/scripts/app.js
@@ -1,14 +1,53 @@
+/* eslint-disable no-undef */
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
-let chosenMaxLife = 100;
+const chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
+// eslint-disable-next-line no-undef
 adjustHealthBars(chosenMaxLife);
 
+function showWinnerMessage(message) {
+  alert(message);
+}
+
+function checkWinner() {
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    showWinnerMessage('You win');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    showWinnerMessage('You lost');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    showWinnerMessage('draw');
+  }
+}
+
+function attackThePlayer() {
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE); // the damage dealt to the player
+  currentPlayerHealth -= playerDamage;
+}
+
+function attackTheMonster(attackMode) {
+  const monsterDamage =
+    attackMode === 'STRONG_ATTACK'
+      ? dealMonsterDamage(STRONG_ATTACK_VALUE)
+      : dealMonsterDamage(ATTACK_VALUE); // the damage dealt to the monster
+  currentMonsterHealth -= monsterDamage;
+}
+
+function strongAttackHandler() {
+  attackTheMonster('STRONG_ATTACK');
+  attackThePlayer();
+  checkWinner();
+}
+
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
+  attackTheMonster('ATTACK');
+  attackThePlayer();
+  checkWinner();
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
